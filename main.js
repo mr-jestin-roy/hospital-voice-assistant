5047c8e50d1258eae435f49f2be4de00f0f56176
fillerInk <thatonephysicsnerd@gmail.com>
2020-11-16 10:57:16 +0530

    Added form and post request

--- main.js
+++ main.js
@@ -24,6 +24,23 @@ app.get('/index',(req,res)=>{
   res.render('index')
 })
 
+app.post('/post',(req,res)=>{
+  //@George, ivide body full aayit print cheythal enter cheytha ellam kittum
+  console.log(req.body)
+
+  //We can also do like this
+  console.log(req.body.name)
+
+  //Here we are calling it by the 'name' tag we gave inside <input> in the html form
+  //avide <input ... name='address' > enn aanenkil we can say req.body.address
+
+  //Ini here we can send all this data to Firebase and store it there
+  // ... 
+
+  //Ennit we send the response back 
+  res.render('index')
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
