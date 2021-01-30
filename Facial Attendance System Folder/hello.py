1e83cc2f9b0cbeb5861ec78007979e4a4d2ac87c
mr-jestin-roy <roy_jestin@yahoo.com>
2021-01-30 19:57:33 +0530

    Streamlit Complete

--- Facial Attendance System Folder/hello.py
+++ Facial Attendance System Folder/hello.py
@@ -2,31 +2,37 @@ import streamlit as st
 # To make things easier later, we're also importing numpy and pandas for
 # working with sample data.
 import numpy as np
+
+from datetime import datetime
 import pandas as pd
 
+danger_html="""  
+      <div style="background-color:#F08080;padding:10px >
+       <h2 style="color:black ;text-align:center;"> Your forest is in danger</h2>
+       </div>
+    """
 
 def main():
     st.title("Team Code Bros")
     st.subheader("Facial Recognition based")
     html_temp = """
-    <div style="background-color:#025246 ;padding:10px">
-    <h2 style="color:white;text-align:center;">ATTENDANCE MANAGEMENT SYSTEM</h2>
-    </div>
-    """
+        <div style="background-color:#025246 ;padding:10px">
+        <h2 style="color:white;text-align:center;">ATTENDANCE MANAGEMENT SYSTEM</h2>
+        </div>
+        """
+    safe_html = """  
+          <div style="background-color:#F4D03F;padding:10px >
+           <h2 style="color:white;text-align:center;"> Updated Attendance File</h2>
+           </div>
+        """
     st.markdown(html_temp, unsafe_allow_html=True)
     st.image('mllogo.jpeg', width=700)
     oxygen = st.text_input("Name","Type Here")
+    st.file_uploader('File uploader')
+    now = datetime.now()
+    dtString = now.strftime('%d:%B:%Y:%H:%M:%S')
+    print(dtString)
 
-    safe_html="""  
-      <div style="background-color:#F4D03F;padding:10px >
-       <h2 style="color:white;text-align:center;"> Updated Attendance File</h2>
-       </div>
-    """
-    danger_html="""  
-      <div style="background-color:#F08080;padding:10px >
-       <h2 style="color:black ;text-align:center;"> Your forest is in danger</h2>
-       </div>
-    """
 
     if st.button("Start"):
 
