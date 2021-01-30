6a44372fe1c5d1508947a9ebf3405aabb3c05ee7
mr-jestin-roy <roy_jestin@yahoo.com>
2021-01-30 19:32:48 +0530

    Streamlit added

--- Facial Attendance System Folder/hello.py
+++ Facial Attendance System Folder/hello.py
@@ -6,20 +6,20 @@ import pandas as pd
 
 
 def main():
-    st.title("Streamlit Tutorial")
+    st.title("Team Code Bros")
+    st.subheader("Facial Recognition based")
     html_temp = """
     <div style="background-color:#025246 ;padding:10px">
-    <h2 style="color:white;text-align:center;">Forest Fire Prediction ML App </h2>
+    <h2 style="color:white;text-align:center;">ATTENDANCE MANAGEMENT SYSTEM</h2>
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
+    st.image('mllogo.jpeg', width=700)
+    oxygen = st.text_input("Name","Type Here")
 
-    oxygen = st.text_input("Oxygen","Type Here")
-    humidity = st.text_input("Humidity","Type Here")
-    temperature = st.text_input("Temperature","Type Here")
     safe_html="""  
       <div style="background-color:#F4D03F;padding:10px >
-       <h2 style="color:white;text-align:center;"> Your forest is safe</h2>
+       <h2 style="color:white;text-align:center;"> Updated Attendance File</h2>
        </div>
     """
     danger_html="""  
@@ -28,14 +28,12 @@ def main():
        </div>
     """
 
-    if st.button("Predict"):
-        output=predict_forest(oxygen,humidity,temperature)
-        st.success('The probability of fire taking place is {}'.format(output))
+    if st.button("Start"):
 
-        if output > 0.5:
-            st.markdown(danger_html,unsafe_allow_html=True)
-        else:
-            st.markdown(safe_html,unsafe_allow_html=True)
+        st.success('Attendance is marked in Attendance.csv')
+        df = pd.read_csv("Attendance.csv")  # read a CSV file
+        st.markdown(safe_html,unsafe_allow_html=True)
+        st.write(df)
 
 if __name__=='__main__':
     main()
